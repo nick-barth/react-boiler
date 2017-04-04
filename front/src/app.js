d5aa121a358086495272fac87d5380f7dfed142d
Nick Barth <barth.nicholas@gmail.com>
2017-04-04 23:12:42 +0200

    this show is terrible

--- front/src/app.js
+++ front/src/app.js
@@ -4,12 +4,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from 'components/SearchBar';
+
 /*
- * Bootstrap the app
+ * A we bit lazy, but I'm going to use this as the main route.
  */
 ReactDOM.render(
-	<div>
-		suck my ass
+	<div className="layout">
+		<div className="layout__container">
+			<div className="layout__title">
+				The Movie Thing
+			</div>
+			<SearchBar />
+		</div>
 	</div>,
 	document.getElementById('app')
 );
--- front/src/components/searchbar/index.js
+++ front/src/components/searchbar/index.js
@@ -5,54 +5,25 @@
 // Vendors
 import React from 'react';
 
-// Components
-import Icon, { shape as iconShape } from 'components/icon';
-
 /*
  * TEXT
  * ====
  */
-class Text extends React.Component {
-
-	static propTypes = {
-		icon: iconShape,
-		title: React.PropTypes.node,
-		children: React.PropTypes.node,
-		centered: React.PropTypes.bool.isRequired
-	};
-
-	static defaultProps = {
-		centered: false
-	};
+class SearchBar extends React.Component {
 
 	constructor (props) {
 		super(props);
 	}
 
 	render () {
-
-		const { icon, title, children, centered } = this.props;
-
 		return (
-			<div className={`text ${centered ? 'text--centered' : ''}`}>
-				{icon ? (
-					<div className="text__icon">
-						<Icon name={icon} />
-					</div>
-				) : null}
-
-				{title ? (
-					<div className="text__title">
-						{title}
-					</div>
-				) : null}
-
-				<div className="text__body">
-					{children}
+			<div className="searchbar">
+				<div className="searchbar__icon">
 				</div>
+				<input className="searchbar__bar" type="text" name="search" />
 			</div>
 		);
 	}
 }
 
-export default Text;
+export default SearchBar;
