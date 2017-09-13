d4128ee2d906ecf9956be22791d7647baadd7729
Pedro <pedropaulogarcez@gmail.com>
2017-09-13 02:05:07 -0400

    Remvoed unnecessary API import from routes/index.js.

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -5,11 +5,7 @@
 // Vendors
 import React from 'react';
 
-// API
-import API from 'api';
-
 // Components
-import Card from 'components/card/index.js';
 import ChampionGrid from 'components/championGrid/index.js'
 
 /*
@@ -17,10 +13,7 @@ import ChampionGrid from 'components/championGrid/index.js'
  * ==============
  */
 export default class App extends React.Component {
-
-
 	render () {
-
 		return (
 			<ChampionGrid />
 		);
