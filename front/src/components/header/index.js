3c23d02974e4a9404ff938a4fe1719a321a39e38
Pedro <pedropaulogarcez@gmail.com>
2017-09-21 17:51:24 -0400

    Finished setting up navbar for mobile. Added a new icons directory for icons and logo. Renamed many classes to better reflect content and BEM methodology.

--- front/src/components/header/index.js
+++ front/src/components/header/index.js
@@ -4,6 +4,7 @@
 
 // Vendors
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /*
  * HEADER
@@ -19,12 +20,23 @@ class Header extends React.Component {
 
 	render () {
 		return (
-			<div className="header">
-				<img className="header__logo" src={'images/main/logo.svg'}/>
-                <div className="header__logo-text">
-                    <span className="header__logo-emphasis">QUAKE</span>CHAMPSELECT
-                </div>
+			<div className="navbar-container">
+				<div className="navbar-container__navbar">
+					<div className="logo">
+						<img className="logo__img" src={'images/icons/logo.svg'}/>
+						<div className="logo__text">
+							<span className="logo__text_emphasis">QUAKE</span>CHAMPSELECT
+						</div>
+					</div>
+					<Link to={'/'}>
+						<div className="navbar-container__button">
+							<img className="navbar-container__icon" src={'images/icons/champ-grid-icon.svg'}/>
+							ALL CHAMPIONS				
+						</div>
+					</Link>
+				</div>
 			</div>
+			
 		);
 	}
 }
