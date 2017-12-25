991822bdec80b2cd2f8b82683bc571b6af96131d
Pedro <pedropaulogarcez@gmail.com>
2017-12-24 22:11:29 -0500

    Replaced the image in the card component with inline css.

--- front/src/components/card/index.js
+++ front/src/components/card/index.js
@@ -34,10 +34,11 @@ class Card extends React.Component {
 		return (
 			<Link to={`/champion/${formatChampName(champ.name)}`} className="card">
 				<div className="card__overlay" />
-				<div className="card__name">
-					{champ.name}
+				<div style={{ 'background-image': `url("images/card/${formatChampName(champ.name)}.jpg")` }} className="card__image-container">
+					<div className="card__name">
+						{champ.name}
+					</div>
 				</div>
-				<img className="card__bg" src={`images/card/${formatChampName(champ.name)}.jpg`}/>
 				<div className="card__tag-line">
 					{champ.tagline}
 					<div className="card__accent" />
